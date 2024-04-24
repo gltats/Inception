0bbdc7ca6fa8d498718022bc1b607cfd04eb0d97
gltats <tatianasofiagomes831@gmail.com>
2024-04-24 16:54:25 +0200

    lol

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -3,17 +3,16 @@ rm -rf /var/run/mysqld/*
 rm -rf /run/mysqld/*
 mkdir -p /var/run/mysqld
 mkdir -p /run/mysqld
-chown mysql:mysql /var/run/mysqld
-chown mysql:mysql /run/mysqld
-chmod 755 /var/run/mysqld
+chown root:root /var/lib/mysql
+chown root:root /run/mysqld
 
 /etc/init.d/mariadb start
-mariadb -e "CREATE DATABASE IF NOT EXISTS $DBNAME;"
-mariadb -e "CREATE USER IF NOT EXISTS '$DBUSER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-mariadb -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
-mariadb -e "FLUSH PRIVILEGES;"
-# service mariadb stop
+mariadb -u root -e "CREATE DATABASE IF NOT EXISTS $DBNAME;"
+mariadb -u root -e "CREATE USER IF NOT EXISTS '$DBUSER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mariadb -u root -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mariadb -u root -e "FLUSH PRIVILEGES;"
+/etc/init.d/mariadb stop
 # sleep infinity
-exec /etc/init.d/mariadb start
+exec mysqld
 # sleep 2
 # mysqld
