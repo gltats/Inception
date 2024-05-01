94404de66058210268f49edb9a9a526ef3d2cfed
Tatiana Sofia Gomes Lima <gltats@Tatianas-MacBook-Air-2.fritz.box>
2024-05-01 17:05:41 +0200

    secrets added

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -25,12 +25,21 @@ else
     wp --allow-root user update $DBUSER --user_pass=$DB_PASSWORD --display_name="I_Like_Bacon"
 	wp --allow-root --path=/var/www/html theme install generatepress
 	wp --allow-root --path=/var/www/html theme activate generatepress
-    cp wp-config-sample.php wp-config.php
-
 fi
 touch /var/www/html/lock
 chown -R www-data:www-data /var/www/
 chmod -R 777 /var/www/html
-php-fpm7.4 -F
+echo "Adding secrets to wp-config.php"
+cat << 'EOF' >> /var/www/html/wp-config.php
+    define('AUTH_KEY',         'J1fU90>,#E:F^&2&Ay]x2@UZbH.F`T#vM?C$BNj8B1lTJQ;60NmEWhDs>Hhd 8)l');
+    define('SECURE_AUTH_KEY',  '@0u$F7/FpDqV[f>*mvYc4DH>lN[sLrdqVHg-R*aC_?fYp}&h^Tq=D(,F(JD+iwxV');
+    define('LOGGED_IN_KEY',    'AoCrl|28rzx*X;$vr9VpxmaOXJ0Ro gY+H2++|w<{q~3F|6?5y8R(8mQl,,Cft?^');
+    define('NONCE_KEY',        '{I3:_.+9-IX+*-:K^>++?%f|=+:55>->E`y<f(7W1(+elDo-A _q9jtnBZBfPH8:');
+    define('AUTH_SALT',        '^t8Qr[O6h**|h=7H|+6(:Gg>x6kxV3aeL+P(RJ|.@W]iZ[Iu+W91W16spEq4xjdc');
+    define('SECURE_AUTH_SALT', '_;}u[)Tg(y.-t0VY|e$EigS#:}QNr-<~GzR:-|a%zm2,;G,]d9PO-{?A%vk)~MI%');
+    define('LOGGED_IN_SALT',   'Qo%gwfFpX{23diX}vS_p1EPaQVG4Ep/(%j:Ap!6*M5y3u%ou66b<x5:p)x^sKRk0');
+    define('NONCE_SALT',       '0vTf>4a91mCkw7s)ez`*%jGHx~D|xq0X!n|,Z/2[{+?@]t/|`plBx`Pu5jw6@_y&');
+EOF
+echo "done with the secrets, now starting php-fpm"
 
-sleep infinity
+php-fpm7.4 -F
